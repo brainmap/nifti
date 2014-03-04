760a953f72f7b057b08e57fea2e5306bd116b4bb
Rafael Reggiain Manzo <rr.manzo@gmail.com>
2014-03-04 13:31:23 -0300

    NImage documentation

--- lib/nifti/n_image.rb
+++ lib/nifti/n_image.rb
@@ -4,12 +4,40 @@ module NIFTI
   class NImage
     attr_reader :array_image, :dim, :previous_indexes
 
+    # Creates an NImage instance.
+    #
+    # The NImages instance provides a user friendly interface to the NIFTI Image
+    # A NImage is typically built by NObject instance
+    #
+    # === Parameters
+    #
+    # * <tt>array_image</tt> -- The NIFTI image contained on and one dimensional array
+    # * <tt>dim</tt> -- The dimensions array from the NIFTI header.
+    #
+    # === Examples
+    #
+    #   # Creates an NImage to deal with an 9 position array that represents a 3x3 matrix
+    #   img = Nimage.new(Array.new(9,0.0), [2,3,3])
+    #
     def initialize(array_image, dim, previous_indexes=[])
       @array_image = array_image
       @dim = dim
       @previous_indexes = previous_indexes
     end
 
+    # Retrieves an element or partition of the dataset
+    #
+    # === Parameters
+    #
+    # * <tt>index</tt> -- The desired index on the dataset
+    #
+    # === Options
+    #
+    # === Examples
+    #
+    #   img[0][0]
+    #   img[0][0..1]
+    #
     def [](index)
       # Dealing with Ranges is useful when the image represents a tensor
       if (index.is_a?(Fixnum) && index >= self.shape[0]) || (index.is_a?(Range) && index.last >= self.shape[0])
@@ -25,6 +53,19 @@ module NIFTI
       end
     end
 
+    # Set the value for an element of the dataset
+    #
+    # === Parameters
+    #
+    # * <tt>index</tt> -- The desired index on the dataset
+    # * <tt>value</tt> -- The value that the will be set
+    #
+    # === Options
+    #
+    # === Examples
+    #
+    #   img[0][0] = 1.0
+    #
     def []=(index,value)
       if self.shape.count != 1 or index >= self.shape[0]
         raise IndexError.new("You can only set values for array values")
@@ -33,6 +74,12 @@ module NIFTI
       end
     end
 
+    # Dataset shape
+    #
+    # === Examples
+    #
+    #   img.shape
+    #
     def shape
       start_index = 1
       self.previous_indexes.each {start_index += 1}
